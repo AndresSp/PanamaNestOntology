70d3effbb6cf7395b2be5ec7f142ee3264a9997d
AndresSp <andrestunonsp@gmail.com>
2017-12-02 15:37:30 -0500

    Backup de Data y creación del nuevo proyecto en VS

---
+++ FusekiQueries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FusekiConnection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
