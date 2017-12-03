7745b5a7325fea519028c6e6524720b1eb8c4136
Omar Jesús Mejía <omargotico@hotmail.com>
2017-12-03 15:17:51 -0500

    Cambios al controller Ontology

---
+++ Core/Model/ResultResponseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Model
+{
+    public class ResultResponseModel
+    {
+        public object data { get; set; }
+        public Error error { get; set; }
+    }
+
+    public class Error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+
+        public Error(int code, string message)
+        {
+            this.code = code;
+            this.message = message;
+        }
+    }
+}
