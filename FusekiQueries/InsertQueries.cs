2b8ec16adde063c20c645e3bf676bcca357e4795
AndresSp <andrestunonsp@gmail.com>
2017-12-04 09:26:23 -0500

    Checking

---
+++ FusekiQueries/InsertQueries.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Storage;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using Entities.Model;
+
+namespace FusekiConnection
+{
+    public class InsertQueries
+    {
+        SparqlUtilities sparqlUtilities;
+        FusekiConnector fuseki;
+        PersistentTripleStore store;
+
+        public InsertQueries()
+        {
+            sparqlUtilities = new SparqlUtilities();
+            fuseki = new FusekiConnector(sparqlUtilities.GraphPath);
+            store = new PersistentTripleStore(fuseki);
+
+            
+        }
+
+        public bool CreateBird(Bird bird)
+        {
+            foreach(var triple in store.Triples)
+            {
+                //Console.WriteLine(store.);
+            }
+            /*
+            Graph graph = new Graph();
+            fuseki.LoadGraph(graph, );
+
+            fuseki.SaveGraph(graph);*/
+            return true;
+        }
+    }
+}
